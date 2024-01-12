3b52ce0ade1d528781ea73801a4e1c4a49ccd592
Charaf Eddine EL KIHAL <charafeddinelkihal@gmail.com>
2024-01-12 01:19:01 +0100

    second commit+

--- src/main/java/com/emse/spring/automacorp/Controllers/RoomController.java
+++ src/main/java/com/emse/spring/automacorp/Controllers/RoomController.java
@@ -2,65 +2,118 @@ package com.emse.spring.automacorp.Controllers;
 
 import com.emse.spring.automacorp.Mappers.RoomMapper;
 import com.emse.spring.automacorp.dao.RoomDao;
+import com.emse.spring.automacorp.dao.SensorDao;
+import com.emse.spring.automacorp.dao.WindowDao;
 import com.emse.spring.automacorp.dto.Room;
+import com.emse.spring.automacorp.dto.RoomCommand;
 import com.emse.spring.automacorp.model.RoomEntity;
+import com.emse.spring.automacorp.model.SensorEntity;
+import com.emse.spring.automacorp.model.SensorType;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = { "http://localhost:5173" }, maxAge = 3600)
+@CrossOrigin
 @RestController
 @RequestMapping("/api/rooms")
 @Transactional
 public class RoomController {
-    private final RoomDao roomDao;
 
-    public RoomController(RoomDao roomDao) {
+    private final RoomDao roomDao;
+    private final WindowDao windowDao;
+
+
+    private final SensorDao sensorDao;
+
+    private static Logger logger = LogManager.getLogger(RoomController.class);
+
+    public RoomController(RoomDao roomDao, WindowDao windowDao,  SensorDao sensorDao) {
         this.roomDao = roomDao;
+        this.windowDao = windowDao;
+
+        this.sensorDao = sensorDao;
     }
 
+    /**
+     * Often the first function called during the acces to this API
+     * @return List of all rooms
+     */
     @GetMapping
-    public List<Room> findAllRooms() {
-        return roomDao.findAll().stream()
+    public List<Room> findAll() {
+        return roomDao.findAll()
+                .stream()
                 .map(RoomMapper::of)
+                .sorted(Comparator.comparing(Room::name))
                 .collect(Collectors.toList());
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Room> findRoomById(@PathVariable Long id) {
-        return roomDao.findById(id)
-                .map(roomEntity -> ResponseEntity.ok(RoomMapper.of(roomEntity)))
-                .orElse(ResponseEntity.notFound().build());
+
+    @GetMapping(path = "/{id}")
+    public Room findById(@PathVariable Long id) {
+        return roomDao.findById(id).map(RoomMapper::of).orElse(null);
     }
+
 
     @PostMapping
-    public ResponseEntity<Room> createRoom(@RequestBody Room room) {
-        RoomEntity roomEntity = RoomMapper.createRoomEntity(room);
-        Room createdRoom = RoomMapper.of(roomDao.save(roomEntity));
-        return ResponseEntity.ok(createdRoom);
+    public ResponseEntity<Room> create(@RequestBody RoomCommand room) {
+
+
+        SensorEntity currentTemperature = new SensorEntity(SensorType.TEMPERATURE,room.name() + " current temperature");
+        currentTemperature.setValue(room.currentTemperature().getValue());
+        sensorDao.save(currentTemperature);
+
+        RoomEntity entity = new RoomEntity( room.getName(), currentTemperature,room.getFloor(), room.targetTemperature() );
+        RoomEntity saved = roomDao.save(entity);
+        return ResponseEntity.ok(RoomMapper.of(saved));
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Room> updateRoom(@PathVariable Long id, @RequestBody Room room) {
-        return roomDao.findById(id)
-                .map(existingRoomEntity -> {
-                    RoomMapper.updateRoomEntity(existingRoomEntity, room);
-                    Room updatedRoom = RoomMapper.of(roomDao.save(existingRoomEntity));
-                    return ResponseEntity.ok(updatedRoom);
-                })
-                .orElse(ResponseEntity.notFound().build());
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<Room> update(@PathVariable Long id, @RequestBody RoomCommand room) {
+        if (id==null && room==null){
+            logger.fatal("There are no id nor room...");
+            return ResponseEntity.badRequest().build();
+        }
+        else if (id==null || room==null) {
+            logger.error("There is a missing argument between id and room.");
+            return ResponseEntity.badRequest().build();
+        }
+        else {
+            RoomEntity entity = roomDao.findById(id).orElse(null);
+            if (entity == null) {
+                logger.warn("The given room id (" + id +")doesn't exist.");
+                return ResponseEntity.badRequest().build();
+            }
+            logger.debug("Beginning of room " + id + "'s update");
+
+            SensorEntity currentTemperature = sensorDao.findById(entity.getCurrentTemperature().getId()).orElse(null);
+            if (currentTemperature == null) {
+                return ResponseEntity.badRequest().build();
+            }
+            currentTemperature.setValue(room.currentTemperature().getValue());
+
+            entity.setFloor(room.floor());
+            entity.setName(room.name());
+            entity.setTargetTemperature(room.targetTemperature());
+            logger.info("The room " + id + " has successfully been modified.");
+            return ResponseEntity.ok(RoomMapper.of(entity));
+        }
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteRoom(@PathVariable Long id) {
-        return roomDao.findById(id)
-                .map(existingRoomEntity -> {
-                    roomDao.delete(existingRoomEntity);
-                    return ResponseEntity.ok().build();
-                })
-                .orElse(ResponseEntity.notFound().build());
+
+    @DeleteMapping(path = "/{id}")
+    public void delete(@PathVariable Long id) {
+        sensorDao.deleteByRoomId(id);
+        windowDao.deleteWindowsByRoomId(id);
+        roomDao.deleteById(id);
     }
+
+
+
 }
--- src/main/java/com/emse/spring/automacorp/Controllers/WindowController.java
+++ src/main/java/com/emse/spring/automacorp/Controllers/WindowController.java
@@ -4,89 +4,94 @@ import com.emse.spring.automacorp.Mappers.SensorMapper;
 import com.emse.spring.automacorp.Mappers.WindowMapper;
 import com.emse.spring.automacorp.dao.RoomDao;
 import com.emse.spring.automacorp.dao.RoomDaoImpl;
+import com.emse.spring.automacorp.dao.SensorDao;
 import com.emse.spring.automacorp.dao.WindowDao;
 import com.emse.spring.automacorp.dto.Window;
 import com.emse.spring.automacorp.dto.WindowCommand;
 import com.emse.spring.automacorp.model.RoomEntity;
 import com.emse.spring.automacorp.model.SensorEntity;
+import com.emse.spring.automacorp.model.SensorType;
 import com.emse.spring.automacorp.model.WindowEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = { "http://localhost:5173" }, maxAge = 3600)@RestController
+@CrossOrigin
+@RestController
 @RequestMapping("/api/windows")
 @Transactional
 public class WindowController {
     private final WindowDao windowDao;
+    private final RoomDao roomDao;
+    private final SensorDao sensorDao;
 
-    public WindowController(WindowDao windowDao) {
+    public WindowController(WindowDao windowDao, RoomDao roomDao, SensorDao sensorDao) {
         this.windowDao = windowDao;
+        this.roomDao = roomDao;
+        this.sensorDao = sensorDao;
     }
 
+
     @GetMapping
-    public List<Window> findAllWindows() {
-        return windowDao.findAll().stream()
-                .map(WindowMapper::toDTO)
+    public List<Window> findAll() {
+        return windowDao.findAll()
+                .stream()
+                .map(WindowMapper::of)
+                .sorted(Comparator.comparing(Window::name))
                 .collect(Collectors.toList());
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Window> findWindowById(@PathVariable Long id) {
-        return windowDao.findById(id)
-                .map(windowEntity -> ResponseEntity.ok(WindowMapper.toDTO(windowEntity)))
-                .orElse(ResponseEntity.notFound().build());
+    @GetMapping(path = "/{id}")
+    public Window findById(@PathVariable Long id) {
+        return windowDao.findById(id).map(WindowMapper::of).orElse(null);
     }
 
     @PostMapping
-    public ResponseEntity<Window> createWindow(@RequestBody WindowCommand windowCommand) {
-        SensorEntity sensorEntity = SensorMapper.createSensorEntity(windowCommand.getWindowStatus());
-        WindowEntity windowEntity = createWindowEntity(windowCommand, sensorEntity);
-        Window createdWindow = WindowMapper.toDTO(windowEntity);
-        return ResponseEntity.ok(createdWindow);
+    public ResponseEntity<Window> create(@RequestBody WindowCommand window) {
+        RoomEntity room = roomDao.findById(window.getRoomId()).orElse(null);
+
+        if (room==null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        SensorEntity windowStatus = new SensorEntity(SensorType.STATUS, window.getName() + " status");
+        windowStatus.setValue(window.getWindowStatus().getValue());
+        sensorDao.save(windowStatus);
+
+        WindowEntity entity = new WindowEntity(window.getName(), windowStatus, room);
+        WindowEntity saved = windowDao.save(entity);
+        return ResponseEntity.ok(WindowMapper.of(saved));
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Window> updateWindow(@PathVariable Long id, @RequestBody WindowCommand windowCommand) {
-        return windowDao.findById(id)
-                .map(existingWindowEntity -> {
-                    SensorEntity sensorEntity = SensorMapper.createSensorEntity(windowCommand.getWindowStatus());
-                    updateWindowEntity(existingWindowEntity, windowCommand, sensorEntity);
-                    Window updatedWindow = WindowMapper.toDTO(existingWindowEntity);
-                    return ResponseEntity.ok(updatedWindow);
-                })
-                .orElse(ResponseEntity.notFound().build());
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<Window> update(@PathVariable Long id, @RequestBody WindowCommand window) {
+        WindowEntity entity = windowDao.findById(id).orElse(null);
+        RoomEntity room = roomDao.findById(window.getRoomId()).orElse(null);
+
+        if (entity == null || room==null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        SensorEntity windowStatus = new SensorEntity(SensorType.STATUS, window.getName() + " status");
+        windowStatus.setValue(window.getWindowStatus().getValue());
+        sensorDao.save(windowStatus);
+
+        entity.setName(window.getName());
+        entity.setWindowStatus(windowStatus);
+        entity.setRoom(room);
+        return ResponseEntity.ok(WindowMapper.of(entity));
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteWindow(@PathVariable Long id) {
-        return windowDao.findById(id)
-                .map(existingWindowEntity -> {
-                    windowDao.delete(existingWindowEntity);
-                    return ResponseEntity.ok().build();
-                })
-                .orElse(ResponseEntity.notFound().build());
+    @DeleteMapping(path = "/{id}")
+    public void delete(@PathVariable Long id) {
+        sensorDao.deleteByWindowId(id);
+        windowDao.deleteById(id);
     }
 
-    // Helper method to create WindowEntity from WindowCommand
-    private WindowEntity createWindowEntity(WindowCommand windowCommand, SensorEntity sensorEntity) {RoomDao roomDao;
 
-        RoomEntity roomEntity = new RoomEntity(); // Replace with your logic to get or create RoomEntity
 
-        return new WindowEntity(
-                windowCommand.getName(),
-                sensorEntity,
-                roomEntity
-        );
-    }
-
-    // Helper method to update WindowEntity with WindowCommand
-    private void updateWindowEntity(WindowEntity windowEntity, WindowCommand windowCommand, SensorEntity sensorEntity) {
-        windowEntity.setName(windowCommand.getName());
-        windowEntity.setWindowStatus(sensorEntity);
-        // Update other properties if needed
-    }
 }
